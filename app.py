89f53bba9220ca921b6229053a674bb195f17fb3
tonypnode <sgtp@me.com>
2019-05-18 14:54:18 -0400

    Add IP Address report
    Signed-off-by: tonypnode <sgtp@me.com>

--- app.py
+++ app.py
@@ -8,9 +8,27 @@ def vt_api_post(file_hash, api_key):
     params['resource'] = file_hash
     return requests.post(url, params)
 
+
+def vt_api_post_ip_report(ip_address, api_key):
+    params = {'apikey': api_key}
+    params['ip'] = ip_address
+    url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
+    return requests.get(url, params)
+
+
 @action
 def is_file_hash_malicious(file_hash, api_key):
     result = vt_api_post(file_hash, api_key)
     resultObject = result.json()
     maliciousConfidence = resultObject["positives"]/resultObject["total"]
     return {"MaliciousConfidence": maliciousConfidence, "Report": resultObject}, "Report"
+
+
+@action
+def ip_report(ip_address, api_key):
+    # TODO: Add IP address validation
+    result = vt_api_post_ip_report(ip_address, api_key)
+    resultObject = result.json()
+    ip_report_result = resultObject
+    return ip_report_result, "IPAddressReport"
+
