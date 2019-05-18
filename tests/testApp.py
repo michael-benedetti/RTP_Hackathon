63ebbbd920e8acdf3b567897050b3c5167616a49
Anthony Butt <butt.anthonyj@gmail.com>
2019-05-18 13:36:34 -0400

    Adds WannaCryMD5Hash

--- tests/testApp.py
+++ tests/testApp.py
@@ -7,10 +7,12 @@ import app
 class TestApp(TestCase):
 
     def test_isFileMalicious(self):
+        wannaCryMd5Hash = 'db349b97c37d22f5ea1d1841e3c89eb4'
         hasher = hashlib.md5()
-        with open('../testFiles/eicar.com.txt') as afile:
+        with open('../testFiles/eicar.com.txt', 'rb') as afile:
             buf = afile.read()
             hasher.update(buf)
         hash = hasher.hexdigest()
 
         self.assertTrue(app.isFileHashMalicious(hash))
+        self.assertTrue(app.isFileHashMalicious(wannaCryMd5Hash))
