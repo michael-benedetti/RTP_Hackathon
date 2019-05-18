a6d8f6fae5134f2b0b30f337534f04a539f96cee
Michael Benedetti <benedettim@forcenexus.io>
2019-05-18 13:36:44 -0400

    Provided hash and API key, can return malicious confidence

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import logging
+import requests
 
 from apps import App, action
 
 logger = logging.getLogger("apps")
 
-# def vt_api_post(file_hash, api_key):
-#     params = {'apikey': api_key}
-#     url = 'https://www.virustotal.com/vtapi/v2/file/report'
-#     params['resource'] = file_hash
-#     requests.post(url, params)
-#     if api_key == "":
-#         return True
-#     else:
-#         return False
+def vt_api_post(file_hash, api_key):
+    params = {'apikey': api_key}
+    url = 'https://www.virustotal.com/vtapi/v2/file/report'
+    params['resource'] = file_hash
+    return requests.post(url, params)
 
 @action
-def is_file_malicious(file_hash, api_key):
-    logger.debug("file_hash: {}".format(file_hash))
-    logger.debug("file_hash: {}".format(api_key))
-    return True, "Malicious"
+def is_file_hash_malicious(file_hash, api_key):
+    result = vt_api_post(file_hash, api_key)
+    logger.debug("result: {}".format(result.json()))
+    resultObject = result.json()
+    maliciousConfidence = resultObject["positives"]/resultObject["total"]
+    return maliciousConfidence, "MaliciousConfidence"
