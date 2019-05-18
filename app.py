997113de659cffba35138223ad8f3910d85a435a
Michael Benedetti <benedettim@forcenexus.io>
2019-05-18 13:04:02 -0400

    WIP - Ability to hit VT api

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 import logging
-import socket
-import time
 
 from apps import App, action
 
 logger = logging.getLogger("apps")
 
+# def vt_api_post(file_hash, api_key):
+#     params = {'apikey': api_key}
+#     url = 'https://www.virustotal.com/vtapi/v2/file/report'
+#     params['resource'] = file_hash
+#     requests.post(url, params)
+#     if api_key == "":
+#         return True
+#     else:
+#         return False
 
 @action
-def test():
-    logger.debug("This is a another test from {}".format(socket.gethostname()))
-    return {"message": "FN Agile FROM {}".format(socket.gethostname())}
-
-class Test(App):
-    """This app defines the same actions as above, but bound to an app instance. This instance will keep track fo how
-    many total actions are called for this app's instance.
-    """
-
-    def __init__(self, name, device, context):
-        App.__init__(self, name, device, context)
-        # Functions and Variables that are designed to exist across functions go here
-        self.introMessage = {"message": "HELLO WORLD FROM {}".format(socket.gethostname())}
-        self.total_called_functions = 0
-
-
-    def shutdown(self):
-        return
+def is_file_malicious(file_hash, api_key):
+    logger.debug("file_hash: {}".format(file_hash))
+    logger.debug("file_hash: {}".format(api_key))
+    return True, "Malicious"
